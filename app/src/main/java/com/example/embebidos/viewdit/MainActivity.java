ee4f03b10568d3273ca6f944e8695f97b2852abe
MrJaguar <guajardo_luis@outlook.com>
2016-11-19 01:29:57 -0600

    Solo corregi en bitmap

--- app/src/main/java/com/example/embebidos/viewdit/MainActivity.java
+++ app/src/main/java/com/example/embebidos/viewdit/MainActivity.java
@@ -12,6 +12,12 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.widget.ImageView;
+
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -86,10 +92,13 @@ public class MainActivity extends AppCompatActivity {
             return;
 
         Bitmap bitmap = null;
+        Bitmap bitmap2 = null;
         try {
             bitmap = MediaStore.Images.Media.getBitmap( getApplicationContext().getContentResolver(), fotoUri);
+
+            bitmap2 = MatToBit(GrayScale(BitToMat(bitmap),bitmap.getWidth(),bitmap.getHeight()),bitmap.getWidth(),bitmap.getHeight());
             campoFoto = (ImageView) findViewById(R.id.imageContent);
-            campoFoto.setImageBitmap(bitmap);
+            campoFoto.setImageBitmap(bitmap2);
             //AlertDialog.Builder b = new AlertDialog.Builder(this);
             //b.show();
         } catch (IOException e) {
@@ -111,6 +120,53 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
+    }
+
+    private int[][][] BitToMat(Bitmap bmp)
+    {
+        int picw = bmp.getWidth(); int pich = bmp.getHeight();
+        int[] pix = new int[picw * pich];
+        bmp.getPixels(pix, 0, picw, 0, 0, picw, pich);
+        int matriz[][][] = new int[picw][pich][4];
+        for (int y = 0; y < pich; y++)
+            for (int x = 0; x < picw; x++)
+            {
+                int index = y * picw + x;
+                matriz[x][y][0] = (pix[index] >> 24) & 0xff;
+                matriz[x][y][1] = (pix[index] >> 16) & 0xff;
+                matriz[x][y][2] = (pix[index] >> 8) & 0xff;
+                matriz[x][y][3] = pix[index] & 0xff;
+            }
+        return matriz;
+    }
+
+    private int[][][] GrayScale(int mat[][][], int w, int h)
+    {
+        int matriz[][][] = new int[w][h][4];
+        for (int y = 0; y < h; y++)
+            for (int x = 0; x < w; x++)
+            {
+                int r = mat[x][y][1];
+                int g = mat[x][y][2];
+                int b = mat[x][y][3];
+                int R = (int)(0.299*r + 0.587*g + 0.114*b);
+                matriz[x][y][1] = R; matriz[x][y][2] = R; matriz[x][y][3] = R;
+            }
+        return matriz;
+    }
+
+    private Bitmap MatToBit(int mat[][][], int w, int h)
+    {
+        int[] pix = new int[w * h];
+        for (int y = 0; y < h; y++)
+            for (int x = 0; x < w; x++)
+            {
+                int index = y * w + x;
+                pix[index] = 0xff000000 | (mat[x][y][1] << 16) | (mat[x][y][2] << 8) | mat[x][y][3];
+            }
+        Bitmap bm = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_4444);
+        bm.setPixels(pix, 0, w, 0, 0, w, h);
+        return bm;
     }
 
 }
